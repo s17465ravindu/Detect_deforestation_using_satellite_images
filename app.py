2a8e0426c64e8edce71a3e8ae9173ee8b12bd003
s17465ravindu <117331506+s17465ravindu@users.noreply.github.com>
2023-08-18 11:30:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-#import rasterio 
+import rasterio 
 #from sklearn.linear_model import LogisticRegression
 #from sklearn.model_selection import train_test_split
 #from sklearn.metrics import plot_confusion_matrix, plot_roc_curve, plot_precision_recall_curve
